d080ab6c0c06e5b37e30a381fff84b40486e1174
Paulo Fernando <paulo.fdo@hotmail.com>
2021-01-02 15:55:29 -0300

    Removido Log.

--- SampleApi/Controllers/WeatherForecastController.cs
+++ SampleApi/Controllers/WeatherForecastController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SampleApi.Controllers
 {
@@ -15,14 +13,7 @@ namespace SampleApi.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
+ 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
